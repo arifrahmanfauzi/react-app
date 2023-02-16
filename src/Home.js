1f77d601abd17ee7016d1f99ef8efd449b3ef4f8
arif rahman fauzi <arif.rahman1780@gmail.com>
2023-02-16 21:41:01 +0700

    using env

---
+++ src/Home.js
@@ -0,0 +1,45 @@
+import {Button, Card, Container, Row, Col,} from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <div>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <h2>{process.env.REACT_APP_URL}</h2>
+                    </Col>
+                    <Col>test</Col>
+                </Row>
+            </Container>
+            {/*<Container className="m-2">*/}
+            {/*    <Card>*/}
+            {/*        <Card.Body>*/}
+            {/*            <Card.Title>Card Title</Card.Title>*/}
+            {/*            <Card.Text>*/}
+            {/*                Some quick example text to build on the card title and make up the*/}
+            {/*                bulk of the card's content.*/}
+            {/*            </Card.Text>*/}
+            {/*            <Button variant="primary">Go somewhere</Button>*/}
+            {/*        </Card.Body>*/}
+            {/*    </Card>*/}
+            {/*</Container>*/}
+            {/*<h2>Home Page</h2>*/}
+
+            {/*<Card style={{ width: '18rem' }}>*/}
+            {/*    <Card.Img variant="top" src="holder.js/100px180" />*/}
+            {/*    <Card.Body>*/}
+            {/*        <Card.Title>Card Title</Card.Title>*/}
+            {/*        <Card.Text>*/}
+            {/*            Some quick example text to build on the card title and make up the*/}
+            {/*            bulk of the card's content.*/}
+            {/*        </Card.Text>*/}
+            {/*        <Button variant="primary">Go somewhere</Button>*/}
+            {/*    </Card.Body>*/}
+            {/*</Card>*/}
+            {/*<Button href={process.env.REACT_APP_URL}>{process.env.REACT_APP_URL}</Button>*/}
+            {/*<Button href="/profile">Ke Profile</Button>*/}
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import {Button} from "react-bootstrap";
+
+const Profile = () => {
+    return (
+        <div>
+            <h2>Profile Page</h2>
+            <Button href="/">Home</Button>
+        </div>
+    )
+}
+
+export default Profile
